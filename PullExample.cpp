6611023b637c5c85f6235a534b977ef37177d6af
HoriKyouko <powerkilroy@gmail.com>
2019-05-18 14:48:21 -0400

    Update PullExample.cpp

--- PullExample.cpp
+++ PullExample.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Now were going to create a pull example for GitHub Desktop" << std::endl;
+    std::cout << "Now were going to create a pull example for GitHub Desktop!" << std::endl;
     return 0;
 }
