581a779070bca2f2271543fe751e94a51b0c3ab7
Hori <powerkilroy@gmail.com>
2019-05-18 21:05:23 -0400

    Update HelloGitHub.cpp

--- HelloGitHub.cpp
+++ HelloGitHub.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello GitHub Desktop!" << std::endl;
+
     std::cout << "Now were in the BranchExample Branch we just created!" << std::endl;
     return 0;
 }
